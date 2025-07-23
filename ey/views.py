aa2d118becaafad2f1ec8d898d92283109bd8ecf
Suryaraju3 <suryaraju485@gmail.com>
2025-07-23 09:40:51 +0530

    updated

--- ey/views.py
+++ ey/views.py
@@ -5,17 +5,9 @@ from .models import PatientDetails,Doctors,Pharmacy
 from django.contrib.auth.models import User
 from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth import authenticate
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-
-
-
-from rest_framework.decorators import api_view
-
 
 
 class DoctorList(generics.ListCreateAPIView):
